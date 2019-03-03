fb4a2a2db76b96f3650f95a5e81a183fec0d26e9
golansho <xccxxxccx@gmail.com>
2019-03-03 09:44:02 -0500

    fixmac

--- if.sh
+++ if.sh
@@ -2,7 +2,7 @@
 
 ##############################
 # GolanSho
-# 1.0.0
+# 1.0.2
 # arrnging interfaces in chart
 ##############################
 
@@ -15,10 +15,9 @@ interface3=$(ifconfig |awk -F: '$1>0 {print $1}'|awk 'NR==3'|grep [a-z][a-z][a-z
 ip1=$(ifconfig | grep "[i][n][e][t][ ]" |awk -F" " 'NR==1''{ print $2 }')
 ip2=$(ifconfig | grep "[i][n][e][t][ ]" |awk -F" " 'NR==2''{ print $2 }')
 ip3=$(ifconfig | grep "[i][n][e][t][ ]" |awk -F" " 'NR==3''{ print $2 }')
-mac1=$(ifconfig | grep "[e][t][h][e][r]" | awk -F" " 'NR==1''{print $2}')
-mac2=$(ifconfig | grep "[e][t][h][e][r]" | awk -F" " 'NR==2''{print $2}')
-mac3=$(ifconfig | grep "[e][t][h][e][r]" | awk -F" " 'NR==3''{print $2}')
-
+mac1=$(ifconfig | grep "[e,l][t,o][h,o][e,p]" | awk -F" " 'NR==1''{print $2}')
+mac2=$(ifconfig | grep "[e,l][t,o][h,o][e,p]" | awk -F" " 'NR==2''{print $2}')
+mac3=$(ifconfig | grep "[e,l][t,o][h,o][e,p]" | awk -F" " 'NR==3''{print $2}')
 #########################
 # Functions           ###
 
@@ -40,6 +39,7 @@ fi
 floop1=$(f_loop1)
 floop2=$(f_loop2)
 floop3=$(f_loop3)
+
 #########################
 # Building the chart  ###
 
