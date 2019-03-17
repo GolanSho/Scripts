2a9da285ffb7f903c17bb76a368ec389cb1b4f26
golan <golan@localhost.localdomain>
2019-03-17 10:49:43 -0400

    new script

---
+++ chmod.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+f_chmod(){
+
+read -p "Enter a Full Path: " fpath
+
+list=$(ls $fpath)
+
+for i in $list
+ do
+  
+  if [ -d "$fpath$i" ]; then
+   chmod 775 "$fpath$i" && echo "Dir Changed"
+  elif [ -f "$fpath$i" ]; then
+   chmod 604 "$fpath$i" && echo "File Changed"
+  else echo "cant change that"
+  fi
+done
+}
+
+f_chmod   
